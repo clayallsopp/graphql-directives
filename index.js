ec869c3623f51583f7b76e23dacfadc053ab09de
Clay Allsopp <clay.allsopp@gmail.com>
2016-04-10 21:45:54 -0700

    a custom graphql directive

--- index.js
+++ index.js
@@ -7,8 +7,30 @@ import {
   GraphQLUnionType,
   GraphQLList,
   GraphQLInterfaceType,
-  GraphQLID
+  GraphQLID,
+  GraphQLBoolean
 } from 'graphql';
+
+import { DirectiveLocation, GraphQLDirective,
+  GraphQLIncludeDirective, GraphQLSkipDirective } from 'graphql/type/directives';
+
+const resolveWithInstrumentation = (resolve) => {
+  return (source, args, context, info) => {
+    const directives = info.fieldASTs[0].directives;
+    const instrumentDirective = directives.filter(d => d.name.value === InstrumentDirective.name)[0];
+    if (!instrumentDirective) {
+      return resolve(source, args, context, info);
+    }
+
+    const start = new Date();
+    return Promise.resolve(resolve(source, args, context, info)).then((result) => {
+      const diff = (new Date() - start);
+      const tag = instrumentDirective.arguments[0].value.value;
+      console.log(`Instrumented ${tag} @ ${diff}ms`)
+      return result;
+    });
+  };
+};
 
 const ResultType = new GraphQLObjectType({
   name : 'Result',
@@ -17,36 +39,61 @@ const ResultType = new GraphQLObjectType({
       type : new GraphQLNonNull(GraphQLString)
     },
     id : {
-      type : new GraphQLNonNull(GraphQLID)
+      type : new GraphQLNonNull(GraphQLID),
+      resolve: resolveWithInstrumentation((result) => {
+        return result.id;
+      })
     }
   }
-})
+});
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      search: {
-        type: ResultType,
-        args:  {
-          text: {
-            type : new GraphQLNonNull(GraphQLString)
-          }
-        },
-        resolve(root, args) {
-          const text = args.text;
-          return { text, id: `id:${text}` };
-        }
+const InstrumentDirective = new GraphQLDirective({
+  name: 'instrument',
+  description:
+    'Instrument the time it takes to resolve the field',
+  locations: [
+    DirectiveLocation.FIELD,
+  ],
+  args: {
+    tag: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'A tag to store in the metrics store'
+    }
+  },
+});
+
+
+const RootQueryType = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    search: {
+      type: ResultType,
+      args:  {
+        text: { type : new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(root, args) {
+        const text = args.text;
+        return { text, id: `id:${text}` };
       }
     }
-  })
+  }
+});
+
+
+const schema = new GraphQLSchema({
+  directives: [
+    InstrumentDirective,
+    GraphQLIncludeDirective,
+    GraphQLSkipDirective
+  ],
+  query: RootQueryType
 });
 
 const query = `
   {
     search(text: "cat") {
       text
-      id @include(if: false)
+      id @instrument(tag: "search.id")
     }
   }
 `;
